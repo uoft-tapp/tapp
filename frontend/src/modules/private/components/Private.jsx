2970d922bcbd31035709903816e0ac68323b90d6
Robertson McClure <ephipitau@gmail.com>
2018-10-03 08:49:59 -0400

    form state handling

--- frontend/src/modules/private/components/Private.jsx
+++ frontend/src/modules/private/components/Private.jsx
@@ -2,11 +2,20 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 class Private extends React.Component {
+    state = { name: "" }
+    handleChange = prop => event => this.setState({ [prop]: event.target.value })
     render() {
         return (
             <div>
-                <h1>Private!!!</h1>
+                <h1>
+                    Private
+                    {this.state.name.length > 0 ? ` area for ${this.state.name}` : ""}
+                    !!!
+                </h1>
                 <Link to="/">Return to the Welcome Page</Link>
+                <h2>Form:</h2>
+                <label>Name</label>
+                <input value={this.state.name} onChange={this.handleChange("name")} />
             </div>
         )
     }
