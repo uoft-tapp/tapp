ce18c7cb99bc8ce2d6bcf885dcebba0d5731ac5d
Joyce Woo <yyjoycewoo@gmail.com>
2018-10-09 22:13:01 -0400

    Add api for positions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 # Base class from which most controllers will inherit from
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
 end
