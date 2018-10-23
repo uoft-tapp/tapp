30be5c0f482f402776f435df6b3cd223b548062f
Joyce Woo <yyjoycewoo@gmail.com>
2018-10-22 21:11:16 -0400

    Complete position api rspecs

--- spec/controllers/api/v1/positions_controller_spec.rb
+++ spec/controllers/api/v1/positions_controller_spec.rb
@@ -5,35 +5,23 @@ require 'rails_helper'
 RSpec.describe Api::V1::PositionsController, type: :api do
   let(:round) { FactoryBot.create(:round, :fall) }
   let!(:position) { FactoryBot.create(:position) }
-  # This should return the minimal set of attributes required to create a valid
+
+  # This is the minimal set of attributes required to create a valid
   # Position. As you add validations to Position, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) do
     {
       "openings": 25,
-      "course_code": 'CSC100H1F',
-      "round": round,
-      "course_name": 'Introduction to Computer Science',
-      "current_enrolment": 500,
-      "duties": 'Lead labs',
-      "qualifications": 'Well versed in Python',
-      "hours": 54,
-      "cap_enrolment": 500,
-      "num_waitlisted": 100
+      "round_id": round.id,
+      "course_code": 'CSC108H1F'
     }
   end
 
   let(:invalid_attributes) do
     {
-      "round": round,
-      "course_name": 'Introduction to Computer Science',
-      "current_enrolment": 500,
-      "duties": 'Lead labs',
-      "qualifications": 'Well versed in Python',
-      "hours": 54,
-      "cap_enrolment": 500,
-      "num_waitlisted": 100,
-      "openings": -10
+      "openings": -10,
+      "round_id": round.id,
+      "course_code": 'CSC108H1F'
     }
   end
 
@@ -56,8 +44,7 @@ RSpec.describe Api::V1::PositionsController, type: :api do
     end
 
     it 'returns the correct information for each position' do
-      # expect(json).to eq(position.to_json)
-      skip('add assertions to ensure json attributes are correct')
+      expect(last_response.body).to eq('[' + position.to_json + ']')
     end
   end
 
@@ -71,17 +58,71 @@ RSpec.describe Api::V1::PositionsController, type: :api do
     end
 
     it 'returns the correct information for each position' do
-      # expect(json).to eq(position.to_json)
-      skip('add assertions to ensure json attributes are correct')
+      expect(last_response.body).to eq(position.to_json)
     end
   end
 
   describe 'POST /positions' do
-    # TODO
+    context 'with valid params' do
+      it 'creates a new Position' do
+        expect do
+          post '/api/v1/positions/', valid_attributes, session: valid_session
+        end.to change(Position, :count).by(1)
+      end
+
+      it 'returns a success response' do
+        post '/api/v1/positions/', valid_attributes, session: valid_session
+        expect(last_response).to be_successful
+      end
+    end
+
+    context 'with invalid params' do
+      it 'does not create a new Position' do
+        expect do
+          post '/api/v1/positions/', invalid_attributes, session: valid_session
+        end.to change(Position, :count).by(0)
+      end
+
+      it 'returns an appropriate message' do
+        post '/api/v1/positions/', invalid_attributes, session: valid_session
+        expect(json['openings']).to eq(['must be greater than 0'])
+      end
+    end
   end
 
   describe 'PUT /positions/:id' do
-    # TODO
+    let(:new_attributes) { { qualifications: 'New and updated qualifications' } }
+
+    context 'when record exists' do
+      context 'with valid params' do
+        before(:each) do
+          put "/api/v1/positions/#{position.id}", new_attributes, session: valid_session
+        end
+
+        it 'updates the requested position' do
+          position.reload
+          expect(json['qualifications']).to eq(new_attributes[:qualifications])
+        end
+
+        it 'returns a success response' do
+          expect(last_response).to be_successful
+        end
+      end
+
+      context 'with invalid params' do
+        it 'returns an error message' do
+          put "/api/v1/positions/#{position.id}", invalid_attributes, session: valid_session
+          expect(json['openings']).to eq(['must be greater than 0'])
+        end
+      end
+    end
+
+    context 'when record does not exist' do
+      it 'returns an error message' do
+        put "/api/v1/positions/#{position.id + 1}", new_attributes, session: valid_session
+        expect(json['message']).to eq("Couldn't find Position with 'id'=#{position.id + 1}")
+      end
+    end
   end
 
   describe 'DELETE /positions/:id' do
@@ -89,6 +130,11 @@ RSpec.describe Api::V1::PositionsController, type: :api do
       expect do
         delete "/api/v1/positions/#{position.id}", params: {}, session: valid_session
       end.to change(Position, :count).by(-1)
+    end
+
+    it 'returns a success response' do
+      delete "/api/v1/positions/#{position.id}", params: {}, session: valid_session
+      expect(last_response).to be_successful
     end
   end
 end
