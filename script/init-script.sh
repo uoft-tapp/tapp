92275797d98f099d417cb70264063ca52d753653
Mathew Zaleski <matz@zaleski.ca>
2018-11-01 17:09:27 -0400

    comment only
    I think script is confused

--- script/init-script.sh
+++ script/init-script.sh
@@ -1,3 +1,4 @@
 gem install rubocop
+# is this right? have to be in root of local clone, so path to script is just ./script/pre-commit
 ln -s ../../script/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
