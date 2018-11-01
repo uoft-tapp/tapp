6a5e695407f3029e1d420380cb1ae9d34813bc40
Mathew Zaleski <matz@zaleski.ca>
2018-11-01 17:13:15 -0400

    correct path to ./scripts dir

--- script/init-script.sh
+++ script/init-script.sh
@@ -1,4 +1,3 @@
 gem install rubocop
-# is this right? have to be in root of local clone, so path to script is just ./script/pre-commit
-ln -s ../../script/pre-commit .git/hooks/pre-commit
+ln -s ./script/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
