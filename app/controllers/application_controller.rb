e24913bdb31a4ceb8bcbca756548c87b59ead557
Joyce Woo <yyjoycewoo@gmail.com>
2018-11-14 09:10:09 -0500

    remove unnecessary code (#27)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@
 
 # Base class from which most controllers will inherit from
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
   include Response
   include ExceptionHandler
-  include Authenticable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :positions
-      resources :sessions, :only => [:create, :destroy]
     end
   end
 end
