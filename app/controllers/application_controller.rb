6876cb12a33bc9355e40a671ac3615de4870427b
Joyce Woo <yyjoycewoo@gmail.com>
2018-11-13 21:23:19 -0500

    Add ruby dependency to README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@
 
 # Base class from which most controllers will inherit from
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   include Response
   include ExceptionHandler
+  include Authenticable
 end
