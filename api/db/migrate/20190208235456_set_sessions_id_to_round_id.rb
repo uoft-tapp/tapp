71e7f0394f6df65d703b0588108bd864292c9124
Chris <chris.lessard.96@gmail.com>
2019-02-18 11:40:28 -0500

    bug(api): Fix typo in migration
    This patch corrects a small typo found in one migration.

--- api/db/migrate/20190208235456_set_sessions_id_to_round_id.rb
+++ api/db/migrate/20190208235456_set_sessions_id_to_round_id.rb
@@ -2,6 +2,6 @@ class SetSessionsIdToRoundId < ActiveRecord::Migration[5.2]
   def change
     # Ideally this is done with SQL
     Position.all.each{|p| p.update_attribute(:session_id, p.round.session_id)}
-    add_foreign_key: :positions, :sessions
+    add_foreign_key :positions, :sessions
   end
 end
