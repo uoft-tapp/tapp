63ed2ec85db8504192f83daae00cd3510dde5b88
Chris Lessard <chris.lessard.96@gmail.com>
2019-03-10 16:16:14 -0400

    enhancement(api): Define new route for adding applicants
    This patch introduces a new route through which applicants can be
    POSTed and PUTed.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :positions, :instructors
+      resources :applicants, only: [:create, :update]
     end
   end
 end
