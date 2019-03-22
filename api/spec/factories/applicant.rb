4b9ebf0a317d11e2cd58b3e7c54fd74e74c73bd7
Chris Lessard <chris.lessard.96@gmail.com>
2019-03-22 01:05:41 -0400

    fix(api): Add factories fields for applicant
    This commit adds new fields to the factory component for the applicant
    model.

--- api/spec/factories/applicant.rb
+++ api/spec/factories/applicant.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email("#{first_name} #{last_name}", '') }
     student_number { Faker::Number.number(10) }
+    program { Faker::Lorem.word }
+    dept_fields { Faker::Lorem.word }
     utorid { Faker::Internet.slug("#{last_name} #{first_name} #{Faker::Number.number(2)}", '') }
   end
 end
