3a26653c2266ef89819925ea5896658aa338e6a5
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-05-24 02:35:15 -0400

    fix(api): remove unused routes GET /api/v1/positions/new GET /api/v1/positions/edit GET /api/v1/instructors/new GET /api/v1/instructors/edit

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :positions, :instructors
+      resources :positions, :instructors, except: [:new, :edit]
       resources :applicants, only: [:create, :update]
       resources :assignments, only: [:index, :show, :create, :update]
 	  resources :offers, only: [:index, :show]
