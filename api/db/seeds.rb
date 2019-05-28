1feed44bbcdd39c4aa543cfaffa28fe44d864de4
Jacob <jiahuanglin27@gmail.com>
2019-05-28 10:06:05 -0400

    refactor(app) refactored hard coded user loading into json

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,69 +1,8 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default
-# values. The data can then be loaded with the rails db:seed command (or created alongside the
-# database with db:setup).
-
-
-# a = Applicant.create(
-#   first_name: 'George',
-#   last_name: 'Wu',
-#   utorid: 'wugeorge',
-#   student_number: '1000136937',
-#   email: 'george.wu.sample@mail.utoronto.ca',
-#   phone: '6473879674',
-#   address: '130 St. George Street',
-#   dept: 'Computer Science',
-#   year_in_program: 4,
-#   is_grad_student: false,
-#   is_full_time: true
-# )
-# s = Session.create(
-#   year: 2018,
-#   semester: 'fall',
-#   pay: 42.25,
-#   start_date: DateTime.new(2018, 9, 1),
-#   end_date: DateTime.new(2018, 12, 31),
-# )
-# p = Position.create(
-#   session: s, # session is a ruby obj ref
-#   course_code: 'CSC148H1F',
-#   course_name: 'Introduction to Computer Science',
-#   current_enrolment: 500,
-#   duties: 'Lead labs',
-#   qualifications: 'Well versed in Python',
-#   hours: 54,
-#   cap_enrolment: 500,
-#   num_waitlisted: 100,
-#   start_date: DateTime.new(2018, 9, 10),
-#   end_date: DateTime.new(2018, 12, 10),
-#   openings: 40
-# )
-
-# # perference is assembled by ref
-# Preference.create(
-#   position: p,
-#   applicant: a,
-#   priority: 1
-# )
-# i = Instructor.create(
-#   first_name: 'Paul',
-#   last_name: 'Brown',
-#   email: 'paul.brown.sample@utoronto.ca',
-#   utorid: 'brownpau'
-# )
-
-# p.instructors << i
-
-# user1 = User.create(
-#   utorid: 'brownpau',
-#   role: 'instructor'
-# )
-
-# user2 = User.create(
-#   utorid: 'admintes',
-#   role: 'admin'
-# )
+# Seed data is contained in the json files under seed folder. This file serves as the script of 
+# pasing json file and create tables from the parsed data. The data can then be loaded with the rails db:seed command (or created alongside the
+# database with db:setup). For testing purpose, use db:reset to reload all the table.
 
 seed_data_dir = Rails.root.join('db', 'seed')
 
@@ -119,5 +58,10 @@ instructors = json_data.map do |row|
   end
 end
 
-
-puts applicants
+# users
+data = File.read("#{seed_data_dir}/user.json")
+raise JSON::ParserError.new("the source file is empty") if data.strip.length == 0
+json_data = JSON.parse(data).fetch("user")
+users = json_data.map do |row|
+  User.create!(row.with_indifferent_access)
+end
