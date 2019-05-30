c4b5e5b9c17a3c6b07b8ec1f8e337bd095c6b914
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-05-30 03:02:05 -0400

    feat(db): remove columns from assignments Remove the following attributes from assignments table applicant_id position_id hours start_date end_date status

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_053744) do
+ActiveRecord::Schema.define(version: 2019_05_30_060319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_053744) do
   end
 
   create_table "assignments", force: :cascade do |t|
-    t.bigint "applicant_id"
-    t.bigint "position_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hours"
