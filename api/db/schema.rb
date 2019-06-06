57cf965f32cd14495cf2cc05c23717a8ba300561
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-06-06 16:59:01 -0400

    feat(db): create relation between applicant, application & applicant_data_for_matching added applicant_id, application_id to applicant_data_for_matching table added belongs_to :applicant, :application in applicant_data_for_matching model added has_many :applicant_data_for_matchings in applicant model added has_one :applicant_data_for_matching in application model added a through to applicant from application

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_203328) do
+ActiveRecord::Schema.define(version: 2019_06_06_204206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2019_06_06_203328) do
     t.string "annotation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "applicant_id"
+    t.bigint "application_id"
+    t.index ["applicant_id"], name: "index_applicant_data_for_matchings_on_applicant_id"
+    t.index ["application_id"], name: "index_applicant_data_for_matchings_on_application_id"
   end
 
   create_table "applicants", force: :cascade do |t|
