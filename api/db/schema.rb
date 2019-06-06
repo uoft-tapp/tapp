f5335912256f73c8b10c63beac205d9dcda8d079
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-06-06 12:01:41 -0400

    feat(db): set columns in users to not null set the following columns in users to not null via migration: utorid

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_154829) do
+ActiveRecord::Schema.define(version: 2019_06_06_155913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_154829) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "utorid"
+    t.string "utorid", null: false
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
