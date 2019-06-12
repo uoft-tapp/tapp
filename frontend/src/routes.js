ac66510a3a0c90921989135aa88b5beda4132e6c
Jacob <jiahuanglin27@gmail.com>
2019-06-12 17:03:49 -0400

    refactor(frontend) move components from applicants_positions to applicants

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Positions from "./modules/positions/components/index";
 import Applicants from "./modules/applicants_by_course/components/index";
 import RootRedirect from "./modules/auth/components/RootRedirect";
@@ -8,6 +9,18 @@ import AllUnassigned from "./modules/all_applicants/components/AllUnassigned";
 import Summary from "./modules/tapp_summary/components/Summary";
 import ApplicationForm from "./modules/application/components/ApplicationForm";
 import PositionsApplied from "./modules/applicants_positions/components/PositionsApplied";
+=======
+import Positions from "./modules/positions/components/index"
+import Applicants from "./modules/applicants_by_course/components/index"
+import RootRedirect from "./modules/auth/components/RootRedirect"
+import Screen from "./modules/cp/components/index"
+import NewPosition from "./modules/positions/components/NewPosition"
+import AllAssigned from "./modules/all_applicants/components/AllAssigned"
+import AllUnassigned from "./modules/all_applicants/components/AllUnassigned"
+import Summary from "./modules/tapp_summary/components/Summary"
+import ApplicationForm from "./modules/application/components/ApplicationForm"
+import PositionsApplied from "./modules/application/components/PositionsApplied"
+>>>>>>> refactor(frontend) move components from applicants_positions to applicants
 
 export const openRoutes = [
     {
