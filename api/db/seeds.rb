39d646e2a2cfb9f73ec04979040f3dc6762c8328
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-06-19 15:31:02 -0400

    fix(db): fix inconsistent indentations make all indentation consistent

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,315 +5,315 @@
 # database with db:setup). For testing purpose, use db:reset to reload all the table.
 
 seedData = [
-	{
-		table: User,
-		dataSource: "/user.json",
-		uniqueKeys: [:utorid],
-		hashKeys: [:role],
-		foreignKeys: [],
-	},
-	{
-		table: Instructor,
-		dataSource: "/instructor.json",
-		uniqueKeys: [:utorid],
-		hashKeys: [],
-		foreignKeys: [],
-	},
-	{
-		table: Session,
-		dataSource: "/session.json",
-		uniqueKeys: [:name],
-		hashKeys: [],
-		foreignKeys: [],
-	},
-	{
-		table: PreferenceLevel,
-		dataSource: "/preference_level.json",
-		uniqueKeys: [:preference_level],
-		hashKeys: [],
-		foreignKeys: [],
-	},
-	{
-		table: Position,
-		dataSource: "/position.json",
-		uniqueKeys: [:position_code],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Session,
-				fk: :session_id,
-			},
-			{
-				table: Instructor,
-				fk: :instructors,
-			}
-		],
-	},
-	{
-		table: PositionDataForAd,
-		dataSource: "/position_data_for_ad.json",
-		uniqueKeys: [:position_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Position,
-				fk: :position_id,
-			}
-		],
-	},
-	{
-		table: PositionDataForMatching,
-		dataSource: "/position_data_for_matching.json",
-		uniqueKeys: [:position_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Position,
-				fk: :position_id,
-			}
-		],
-	},
-	{
-		table: PositionTemplate,
-		dataSource: "/position_template.json",
-		uniqueKeys: [:position_type, :session_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Session,
-				fk: :session_id,
-			}
-		],
-	},
-	{
-		table: Applicant,
-		dataSource: "/applicant.json",
-		uniqueKeys: [:utorid, :student_number],
-		hashKeys: [],
-		foreignKeys: [],
-	},
-	{
-		table: Application,
-		dataSource: "/application.json",
-		uniqueKeys: [:comments, :session_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Session,
-				fk: :session_id,
-			}
-		],
-	},
-	{
-		table: ApplicantDataForMatching,
-		dataSource: "/applicant_data_for_matching.json",
-		uniqueKeys: [:applicant_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Applicant,
-				fk: :applicant_id,
-			},
-			{
-				table: Application,
-				fk: :application_id,
-			}
-		],
-	},
-	{
-		table: PositionPreference,
-		dataSource: "/position_preference.json",
-		uniqueKeys: [:application_id, :position_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Position,
-				fk: :position_id,
-			},
-			{
-				table: Application,
-				fk: :application_id,
-			}
-		],
-	},
-	{
-		table: Assignment,
-		dataSource: "/assignment.json",
-		uniqueKeys: [:applicant_id, :position_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Position,
-				fk: :position_id,
-			},
-			{
-				table: Applicant,
-				fk: :applicant_id,
-			}
-		],
-	},
-	{
-		table: WageChunk,
-		dataSource: "/wage_chunk.json",
-		uniqueKeys: [:assignment_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Assignment,
-				fk: :assignment_id,
-			}
-		],
-	},
-	{
-		table: ReportingTag,
-		dataSource: "/reporting_tag.json",
-		uniqueKeys: [:wage_chunk_id, :position_id],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: WageChunk,
-				fk: :wage_chunk_id,
-			},
-			{
-				table: Position,
-				fk: :position_id,
-			}
-		],
-	},
-	{
-		table: Offer,
-		dataSource: "/offer.json",
-		uniqueKeys: [:assignment_id, :emailed_date, :accepted_date, :rejected_date, :withdrawn_date],
-		hashKeys: [],
-		foreignKeys: [
-			{
-				table: Assignment,
-				fk: :assignment_id,
-			}
-		],
-	}
+    {
+        table: User,
+        dataSource: "/user.json",
+        uniqueKeys: [:utorid],
+        hashKeys: [:role],
+        foreignKeys: [],
+    },
+    {
+        table: Instructor,
+        dataSource: "/instructor.json",
+        uniqueKeys: [:utorid],
+        hashKeys: [],
+        foreignKeys: [],
+    },
+    {
+        table: Session,
+        dataSource: "/session.json",
+        uniqueKeys: [:name],
+        hashKeys: [],
+        foreignKeys: [],
+    },
+    {
+        table: PreferenceLevel,
+        dataSource: "/preference_level.json",
+        uniqueKeys: [:preference_level],
+        hashKeys: [],
+        foreignKeys: [],
+    },
+    {
+        table: Position,
+        dataSource: "/position.json",
+        uniqueKeys: [:position_code],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Session,
+                fk: :session_id,
+            },
+            {
+                table: Instructor,
+                fk: :instructors,
+            }
+        ],
+    },
+    {
+        table: PositionDataForAd,
+        dataSource: "/position_data_for_ad.json",
+        uniqueKeys: [:position_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Position,
+                fk: :position_id,
+            }
+        ],
+    },
+    {
+        table: PositionDataForMatching,
+        dataSource: "/position_data_for_matching.json",
+        uniqueKeys: [:position_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Position,
+                fk: :position_id,
+            }
+        ],
+    },
+    {
+        table: PositionTemplate,
+        dataSource: "/position_template.json",
+        uniqueKeys: [:position_type, :session_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Session,
+                fk: :session_id,
+            }
+        ],
+    },
+    {
+        table: Applicant,
+        dataSource: "/applicant.json",
+        uniqueKeys: [:utorid, :student_number],
+        hashKeys: [],
+        foreignKeys: [],
+    },
+    {
+        table: Application,
+        dataSource: "/application.json",
+        uniqueKeys: [:comments, :session_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Session,
+                fk: :session_id,
+            }
+        ],
+    },
+    {
+        table: ApplicantDataForMatching,
+        dataSource: "/applicant_data_for_matching.json",
+        uniqueKeys: [:applicant_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Applicant,
+                fk: :applicant_id,
+            },
+            {
+                table: Application,
+                fk: :application_id,
+            }
+        ],
+    },
+    {
+        table: PositionPreference,
+        dataSource: "/position_preference.json",
+        uniqueKeys: [:application_id, :position_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Position,
+                fk: :position_id,
+            },
+            {
+                table: Application,
+                fk: :application_id,
+            }
+        ],
+    },
+    {
+        table: Assignment,
+        dataSource: "/assignment.json",
+        uniqueKeys: [:applicant_id, :position_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Position,
+                fk: :position_id,
+            },
+            {
+                table: Applicant,
+                fk: :applicant_id,
+            }
+        ],
+    },
+    {
+        table: WageChunk,
+        dataSource: "/wage_chunk.json",
+        uniqueKeys: [:assignment_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Assignment,
+                fk: :assignment_id,
+            }
+        ],
+    },
+    {
+        table: ReportingTag,
+        dataSource: "/reporting_tag.json",
+        uniqueKeys: [:wage_chunk_id, :position_id],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: WageChunk,
+                fk: :wage_chunk_id,
+            },
+            {
+                table: Position,
+                fk: :position_id,
+            }
+        ],
+    },
+    {
+        table: Offer,
+        dataSource: "/offer.json",
+        uniqueKeys: [:assignment_id, :emailed_date, :accepted_date, :rejected_date, :withdrawn_date],
+        hashKeys: [],
+        foreignKeys: [
+            {
+                table: Assignment,
+                fk: :assignment_id,
+            }
+        ],
+    }
 ]
 
 def insert_data(seeds)
-	tables = {}
-	log = []
-	seeds.each do |seed|
-		update = false
-		tables[seed[:table]] = readJson(seed[:dataSource])
-		success = 0
-		tables[seed[:table]].each do |entry|
-			entry = hashEntry(entry, seed[:hashKeys])
-			entry = processFK(entry, seed[:foreignKeys], tables)
-			if not entry
-				break
-			end
-			if seed[:uniqueKeys].length == 0
-				result = seed[:table].create(entry)
-			else
-				unique = getUniqueEntry(entry, seed[:uniqueKeys])
-				tableEntry = seed[:table].find_by(unique)
-				if tableEntry
-					result = tableEntry.update_attributes(entry)
-					update = true
-				else
-					result = seed[:table].create(entry)
-				end
-			end
-			if result
-				success += 1
-			end
-		end
-		if success == tables[seed[:table]].length and success > 0
-			update = update ? "updated" : "inserted"
-			log.push("#{seed[:table].to_s} data #{update}.")
-		else
-			log.push("Some insertions/updates to #{seed[:table].to_s} failed.")
-		end
-	end
-	puts log
+    tables = {}
+    log = []
+    seeds.each do |seed|
+        update = false
+        tables[seed[:table]] = readJson(seed[:dataSource])
+        success = 0
+        tables[seed[:table]].each do |entry|
+            entry = hashEntry(entry, seed[:hashKeys])
+            entry = processFK(entry, seed[:foreignKeys], tables)
+            if not entry
+                break
+            end
+            if seed[:uniqueKeys].length == 0
+                result = seed[:table].create(entry)
+            else
+                unique = getUniqueEntry(entry, seed[:uniqueKeys])
+                tableEntry = seed[:table].find_by(unique)
+                if tableEntry
+                    result = tableEntry.update_attributes(entry)
+                    update = true
+                else
+                    result = seed[:table].create(entry)
+                end
+            end
+            if result
+                success += 1
+            end
+        end
+        if success == tables[seed[:table]].length and success > 0
+            update = update ? "updated" : "inserted"
+            log.push("#{seed[:table].to_s} data #{update}.")
+        else
+            log.push("Some insertions/updates to #{seed[:table].to_s} failed.")
+        end
+    end
+    puts log
 end
 
 def processFK(entry, foreignKeys, tables)
-	foreignKeys.each do |keyVal|
-		fk = keyVal[:fk].to_s
-		if entry.keys.include?(fk)
-			table = keyVal[:table]
-			if fk[-1] == 's'
-				ids = []
-				entry[fk].each do |id|
-					val = fkHelper(id, tables, table)
-					if val
-						ids.push(val)
-					end
-				end
-				entry[fk] = ids
-			else
-				val = fkHelper(entry[fk], tables, table)
-				if not val
-					return nil
-				else
-					entry[fk] = val.id
-				end
-			end
-		end
-	end
-	return entry
+    foreignKeys.each do |keyVal|
+        fk = keyVal[:fk].to_s
+        if entry.keys.include?(fk)
+            table = keyVal[:table]
+            if fk[-1] == 's'
+                ids = []
+                entry[fk].each do |id|
+                    val = fkHelper(id, tables, table)
+                    if val
+                        ids.push(val)
+                    end
+                end
+                entry[fk] = ids
+            else
+                val = fkHelper(entry[fk], tables, table)
+                if not val
+                    return nil
+                else
+                    entry[fk] = val.id
+                end
+            end
+        end
+    end
+    return entry
 end
 
 def fkHelper(id, tables, table)
-	if id >= 0 and id < tables[table].length
-		entry = {}
-		curr = tables[table][id]
-		curr.keys.each do |hashKey|
-			if not curr[hashKey].kind_of?(Array)
-				entry[hashKey] = curr[hashKey]
-			end
-		end
-		parent = table.find_by(entry)
-		if parent
-			return parent
-		else
-			puts "The #{id}th entry in the JSON file for #{table.to_s} was not inserted into the database."
-			return nil
-		end
-	else
-		puts "#{id} is an invalid for the table #{table.to_s}."
-		return nil
-	end	
+    if id >= 0 and id < tables[table].length
+        entry = {}
+        curr = tables[table][id]
+        curr.keys.each do |hashKey|
+            if not curr[hashKey].kind_of?(Array)
+                entry[hashKey] = curr[hashKey]
+            end
+        end
+        parent = table.find_by(entry)
+        if parent
+            return parent
+        else
+            puts "The #{id}th entry in the JSON file for #{table.to_s} was not inserted into the database."
+            return nil
+        end
+    else
+        puts "#{id} is an invalid for the table #{table.to_s}."
+        return nil
+    end 
 end
 
 def hashEntry(entry, hashKeys)
-	hashKeys.each do |keyVal|
-		strKey = keyVal.to_s
-		if entry.keys.include?(strKey)
-			entry[strKey] = entry[strKey].to_sym
-		end
-	end
-	return entry
+    hashKeys.each do |keyVal|
+        strKey = keyVal.to_s
+        if entry.keys.include?(strKey)
+            entry[strKey] = entry[strKey].to_sym
+        end
+    end
+    return entry
 end
 
 def getUniqueEntry(entry, uniqueKeys)
-	new_entry = {}
-	uniqueKeys.each do |keyVal|
-		if entry.keys.include?(keyVal.to_s)
-			new_entry[keyVal] = entry[keyVal.to_s]
-		end
-	end
-	return new_entry
+    new_entry = {}
+    uniqueKeys.each do |keyVal|
+        if entry.keys.include?(keyVal.to_s)
+            new_entry[keyVal] = entry[keyVal.to_s]
+        end
+    end
+    return new_entry
 end
 
 def readJson(file)
-	seed_data_dir = Rails.root.join('db', 'seed')
-	begin
-		data = File.read("#{seed_data_dir}#{file}")
-		return JSON.parse(data)
-	rescue Errno::ENOENT => e
-		puts e
-		return []
-	rescue JSON::ParserError 
-		puts "#{seed_data_dir}#{file} is not a valid JSON file."
-		return []
-	end
+    seed_data_dir = Rails.root.join('db', 'seed')
+    begin
+        data = File.read("#{seed_data_dir}#{file}")
+        return JSON.parse(data)
+    rescue Errno::ENOENT => e
+        puts e
+        return []
+    rescue JSON::ParserError 
+        puts "#{seed_data_dir}#{file} is not a valid JSON file."
+        return []
+    end
 end
 
 insert_data(seedData)
