caff5f1796350e3e480a30f98fdaa935d09798f7
Jason Siefken <siefkenj@gmail.com>
2019-06-22 15:53:24 -0400

    Travis Changes

---
+++ tests/run-unit-tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This script assumes that all docker images are up and running."
+echo "If they are not, please run `docker-compose up` and rerun this script."
+
+# Run all tapp unit tests
+echo "Running Unit Tests..."
+exit 0
