1b61c249b25aa36ed54b4b73dc1f0f988ca6778f
Jason Siefken <siefkenj@gmail.com>
2019-06-22 16:41:26 -0400

    Add eslint checks

--- tests/run-formatting-tests.sh
+++ tests/run-formatting-tests.sh
@@ -4,3 +4,5 @@
 echo "Running Code Formatting Tests..."
 yarn run prettier-check "frontend/src/**/*.js"
 yarn run prettier-check "frontend/src/**/*.jsx"
+yarn run eslint "frontend/src/**/*.js"
+yarn run eslint "frontend/src/**/*.jsx"
