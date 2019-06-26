99f2fb60d7cf7a56115abdcd4084bd1cfd1c112f
michellemtchai <michelle.chai@mail.utoronto.ca>
2019-06-26 00:13:48 -0400

    fix(db): remove preference_level_id & add preference_level removed preference_level from position_preference table and model added preference_level column to position_preference updated erd diagram

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_035320) do
+ActiveRecord::Schema.define(version: 2019_06_26_040415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,14 +135,13 @@ ActiveRecord::Schema.define(version: 2019_06_26_035320) do
 
   create_table "position_preferences", force: :cascade do |t|
     t.bigint "position_id"
-    t.bigint "preference_level_id"
     t.bigint "application_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "preference_level"
     t.index ["application_id"], name: "index_position_preferences_on_application_id"
     t.index ["position_id", "application_id"], name: "index_position_preferences_on_position_id_and_application_id", unique: true
     t.index ["position_id"], name: "index_position_preferences_on_position_id"
-    t.index ["preference_level_id"], name: "index_position_preferences_on_preference_level_id"
   end
 
   create_table "position_templates", force: :cascade do |t|
