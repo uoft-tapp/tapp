3959101ac5fc7281d4441625c9fd0f0e40f7144b
Jason Siefken <siefkenj@gmail.com>
2019-06-27 00:50:53 -0400

    [travis] Return non-zero exit code when run-*-tests.sh scripts fail

--- tests/run-unit-tests.sh
+++ tests/run-unit-tests.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+cd $(git rev-parse --show-toplevel)
+
+# we want to run all formatting tests and
+# return a non-zero exit code only if one of them fails
+EXIT_CODE=0
+
+ErrorHandler () {
+	EXIT_CODE=$(($EXIT_CODE+$?))
+}
+
+trap ErrorHandler ERR
+
 echo 'This script assumes that all docker images are up and running.'
 echo 'If they are not, please run `docker-compose up` and rerun this script.'
 
 # Run all tapp unit tests
 echo "Running Unit Tests..."
+
+exit $EXIT_CODE
