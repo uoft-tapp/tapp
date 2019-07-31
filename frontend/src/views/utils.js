4745f69af810c97b78305efb82550eef475e14ea
dingtaoliu <danny.dingtao.liu@gmail.com>
2019-07-30 20:03:27 -0400

    feat(frontend): added utils file in views directory, which currently exports a connected session selector.

---
+++ frontend/src/views/utils.js
@@ -0,0 +1,24 @@
+import { connect } from "react-redux";
+
+import { 
+    fetchSessions,
+    setActiveSession,
+    sessionsSelector,
+    activeSessionSelector
+} from "../api/actions";
+import { SessionSelect } from "../components/session-select";
+
+let mapStateToProps = state => {
+    return {
+        sessions: sessionsSelector(state),
+        activeSession: activeSessionSelector(state)
+    };
+};
+
+let mapDispatchToProps = { fetchSessions, setActiveSession };
+
+export const ConnectedSessionSelect = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SessionSelect);
+
