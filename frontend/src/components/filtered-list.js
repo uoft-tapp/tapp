6dc2a952441e8cd872f86cc92b4f2d5b0d789738
dingtaoliu <danny.dingtao.liu@gmail.com>
2019-08-04 18:19:06 -0400

    feat(frontend): added functional component 'FilteredList' and made 'PositionList' compatible as a listRenderer

---
+++ frontend/src/components/filtered-list.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function FilteredList(props) {
+
+    const {data, listRenderer, ...rest} = props;
+    const Renderer = listRenderer;
+
+    const filter = (data) => {
+        const filtered = data
+        return filtered
+    };
+
+    const filteredData = filter(data);
+
+    return (
+        <div>
+            <Renderer data={filteredData} {...rest} />
+        </div>
+    );
+
+};
