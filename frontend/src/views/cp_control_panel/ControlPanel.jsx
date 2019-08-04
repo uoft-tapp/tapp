517e5294d0185af299c2ac83a2b163f194c12f6a
dingtaoliu <danny.dingtao.liu@gmail.com>
2019-08-04 18:22:40 -0400

    refactor(frontend): refactored the control panel into a functional component that uses 'FilteredList' to render the offers

---
+++ frontend/src/views/cp_control_panel/ControlPanel.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { PositionsList } from "../../components/positions-list";
+import { FilteredList } from "../../components/filtered-list";
+import { connect } from "react-redux";
+import { positionsSelector } from "../../api/actions";
+import { ConnectedSessionSelect } from "../sessions/ConnectedSessionSelector"
+
+
+function ControlPanel() {
+
+  const ConnectedFilteredList = connect((state, ownProps) => ({
+    data: positionsSelector(state),
+    listRenderer: ownProps.listRenderer
+  }))(FilteredList);
+
+  return (
+    <div>
+      <ConnectedSessionSelect/>
+      <ConnectedFilteredList listRenderer={PositionsList}/>
+    </div>
+  );
+};
+
+export default ControlPanel;
