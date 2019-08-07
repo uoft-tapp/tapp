f46891fab83455ca175ee1965e4f209ded5f2465
Zane <zane.huang@mail.utoronto.ca>
2019-08-07 16:23:07 -0400

    Move new position components from module to components and view

---
+++ frontend/src/components/new-position-form.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { moment } from "moment";
+
+const toNames = ({ first_name, last_name }) => `${first_name} ${last_name}`;
+
+const validNumber = val => (val && isNaN(val) ? "Not a valid number" : false);
+const validDate = val =>
+    (val && moment(val, "YYYY-MM-DD", true).isValid()) || val === ""
+        ? false
+        : "Not a valid date";
+// check for format XXX123H1
+const validCourseCode = val => {
+    val = val.toUpperCase();
+    if (
+        val.length === 8 &&
+        isNaN(val.slice(0, 3)) &&
+        !isNaN(val.slice(3, 6)) &&
+        (val[6] === "H" || val[6] === "Y") &&
+        !isNaN(val[7])
+    ) {
+        return false;
+    } else {
+        return "Not a valid course code";
+    }
+};
+const validInstructor = (val, props) => {
+    const instructors = props.instructors.map(toNames);
+    return instructors.indexOf(val) !== -1 ? false : "Not a valid instructor";
+};
+
+const newPositionFields = [
+    {
+        label: "Course Code",
+        value: "course_code",
+        validate: [validCourseCode],
+        required: true
+    },
+    {
+        label: "Course Name",
+        value: "course_name",
+        validate: [],
+        required: false
+    },
+    {
+        label: "Current Enrolment",
+        value: "cap_enrolment",
+        validate: [validNumber],
+        required: false
+    },
+    { label: "Duties", value: "duties", validate: [], required: false },
+    {
+        label: "Qualifications",
+        value: "qualifications",
+        validate: [],
+        required: false
+    },
+    {
+        label: "Instructor",
+        value: "instructor",
+        validate: [validInstructor],
+        required: true
+    },
+    {
+        label: "Session",
+        value: "session_id",
+        validate: [validNumber],
+        required: true
+    },
+    {
+        label: "Hours",
+        value: "hours",
+        validate: [validNumber],
+        required: false
+    },
+    {
+        label: "Openings",
+        value: "openings",
+        validate: [validNumber],
+        required: true
+    },
+    {
+        label: "Start Date",
+        value: "start_date",
+        validate: [validDate],
+        required: false
+    },
+    {
+        label: "End Date",
+        value: "end_date",
+        validate: [validDate],
+        required: false
+    }
+];
+
+const DefaultInput = ({ label, curValue, onChange, required }) => (
+    <div>
+        <label style={{ width: "100px" }}>
+            {required ? label + "*" : label}
+        </label>
+        <input value={curValue} onChange={onChange} required={required} />
+    </div>
+);
+
+const TextboxInput = ({ label, curValue, onChange, required }) => (
+    <div>
+        <label style={{ width: "100px" }}>
+            {required ? label + "*" : label}
+        </label>
+        <textarea
+            style={{ width: "152px" }}
+            value={curValue}
+            onChange={onChange}
+            required={required}
+        />
+    </div>
+);
+
+const InstructorInput = ({ curValue, label, onChange, required }) => (
+    <div>
+        <label style={{ width: "100px" }}>
+            {required ? label + "*" : label}
+        </label>
+        <input value={curValue} onChange={onChange} required={required} />
+    </div>
+);
+
+const InstructorList = ({ curValue, instructors, setInstructor }) => {
+    const valid = validInstructor(curValue, { instructors });
+    const matchingInstructors = instructors
+        .map(({ first_name, last_name }) => `${first_name} ${last_name}`)
+        .filter(
+            name => name.toLowerCase().indexOf(curValue.toLowerCase()) !== -1
+        );
+    return (
+        <div>
+            {!!valid && (
+                <ul>
+                    {matchingInstructors.map(i => (
+                        <li onClick={() => setInstructor(i)} key={i}>
+                            {i}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+const initialState = {
+    ...newPositionFields.reduce((acc, cur) => ({ ...acc, [cur.value]: "" }), {})
+};
---
+++ frontend/src/views/new_position_form/fillablePosition.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+    createNewPosition,
+    importNewPosition,
+    getCount,
+    importResult
+} from "../actions";
+import { fetchInstructors } from "../../instructors/actions";
+import { Col, Container, Button } from "react-bootstrap";
+import CSVReader from "react-csv-reader";
+
+class NewPosition extends React.Component {
+    componentDidMount() {
+        this.props.fetchInstructors();
+    }
+    state = { ...initialState };
+    handleChange = prop => event =>
+        this.setState({ [prop]: event.target.value });
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.createNewPosition(this.state);
+    };
+    getInvalid = () =>
+        newPositionFields.reduce((acc, cur) => {
+            if (acc) {
+                return acc;
+            }
+            const errorMessage = cur.validate.reduce(
+                (running, validator) =>
+                    running || validator(this.state[cur.value], this.props),
+                false
+            );
+            return errorMessage ? `${cur.label}: ${errorMessage}` : acc;
+        }, false);
+    setInstructor = name => this.setState({ instructor: name });
+
+    handleForce = async data => {
+        let count_before_import = getCount();
+
+        for (var i = 1; i < data.length; i++) {
+            const position = {
+                course_code: data[i][0],
+                course_name: data[i][1],
+                cap_enrolment: data[i][2],
+                duties: data[i][3],
+                qualifications: data[i][4],
+                instructor: data[i][5],
+                session_id: data[i][6],
+                hours: data[i][7],
+                openings: data[i][8],
+                start_date: data[i][9],
+                end_date: data[i][10]
+            };
+            await this.props.importNewPosition(position);
+        }
+        let count_after_import = getCount();
+        let success_imports =
+            count_after_import.SUCCESS - count_before_import.SUCCESS;
+        let failed_imports =
+            count_after_import.FAILS - count_before_import.FAILS;
+
+        this.props.importResult(success_imports, failed_imports);
+    };
+
+    render() {
+        const invalid = this.getInvalid();
+        return (
+            <Container>
+                <Col xs={8} md={{ offset: 1 }}>
+                    <h2>New Position</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        {newPositionFields.map(({ value, label, required }) => {
+                            switch (value) {
+                                case "instructor":
+                                    return (
+                                        <div>
+                                            <InstructorInput
+                                                curValue={this.state[value]}
+                                                required={required}
+                                                key={label}
+                                                label={label}
+                                                value={value}
+                                                onChange={this.handleChange(
+                                                    value
+                                                )}
+                                            />
+                                            {this.state.instructor !== "" && (
+                                                <InstructorList
+                                                    curValue={this.state[value]}
+                                                    setInstructor={
+                                                        this.setInstructor
+                                                    }
+                                                    key="instructorList"
+                                                    instructors={
+                                                        this.props.instructors
+                                                    }
+                                                />
+                                            )}
+                                        </div>
+                                    );
+
+                                case "qualifications":
+                                case "duties":
+                                    return (
+                                        <TextboxInput
+                                            curValue={this.state[value]}
+                                            key={label}
+                                            label={label}
+                                            value={value}
+                                            required={required}
+                                            onChange={this.handleChange(value)}
+                                        />
+                                    );
+                                default:
+                                    return (
+                                        <DefaultInput
+                                            curValue={this.state[value]}
+                                            key={label}
+                                            label={label}
+                                            value={value}
+                                            required={required}
+                                            onChange={this.handleChange(value)}
+                                        />
+                                    );
+                            }
+                        })}
+                        <br />
+                        <CSVReader
+                            cssClass="csv-reader-input"
+                            label="Select CSV file for new position"
+                            onFileLoaded={this.handleForce}
+                            inputStyle={{ color: "red" }}
+                        />
+                        <br />
+                        <Button
+                            variant="primary"
+                            disabled={!!invalid}
+                            type="submit"
+                        >
+                            {invalid || "Save Changes"}
+                        </Button>
+                    </form>
+                </Col>
+            </Container>
+        );
+    }
+}
+
+export default connect(
+    ({
+        ui: {
+            instructors: { list }
+        }
+    }) => ({
+        instructors: list
+    }),
+    { createNewPosition, fetchInstructors, importNewPosition, importResult }
+)(NewPosition);
