664effa6a397b260f5ae1672f18f9d93913f5fde
Zane <zane.huang@mail.utoronto.ca>
2019-08-19 00:41:55 -0400

    Make application form conform to prettier style

--- frontend/src/views/application_form/fillableApplication.js
+++ frontend/src/views/application_form/fillableApplication.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
-import { createNewApplication } from "../../api/actions/application_form"
+import { createNewApplication } from "../../api/actions/application_form";
 import { updateField } from "./actions";
 import {
     DefaultInput,
