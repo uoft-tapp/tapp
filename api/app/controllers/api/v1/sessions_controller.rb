4b0cfc963ae9398222efe12019f9e6aa13b2414c
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-10-31 23:59:34 -0400

    refactor(api): Refactored Session controller

--- api/app/controllers/api/v1/sessions_controller.rb
+++ api/app/controllers/api/v1/sessions_controller.rb
@@ -1,59 +1,61 @@
 # frozen_string_literal: true
 
-module Api::V1
-    # Controller for Sessions
-    class SessionsController < ApplicationController
-        # GET /sessions
-        def index
-            render_success(Session.order(:id))
+# Controller for Sessions
+class Api::V1::SessionsController < ApplicationController
+    before_action :find_session, except: %i[index create]
+
+    # GET /sessions
+    def index
+        render_success Session.order(:id)
+    end
+
+    # POST /sessions
+    def create
+        # if we passed in an id that exists, we want to update
+        if params[:id].present? && find_session
+            update
+            return
         end
 
-        # POST /sessions
-        def create
-            # if we passed in an id that exists, we want to update
-            update && return if params.key?(:id) && Session.exists?(params[:id])
-            # when creating a new session, a name is required
-            params.require(:name)
-            session = Session.new(session_params)
-            if session.save # passes Session model validation
-                render_success(session)
-            else
-                session.destroy!
-                render_error(session.errors.full_messages.join('; '))
-            end
+        session = Session.new(session_params)
+        if session.save!
+            render_success session
+        else
+            render_error session.errors.full_messages.join('; ')
         end
+    end
 
-        def update
-            session = Session.find(params[:id])
-            if session.update_attributes!(session_params)
-                render_success(session)
-            else
-                render_error(session.errors.full_messages.join('; '))
-            end
+    def update
+        if @session.update_attributes!(session_params)
+            render_success @session
+        else
+            render_error @session.errors.full_messages.join('; ')
         end
+    end
 
-        # POST /sessions/delete
-        def delete
-            params.require(:id)
-            session = Session.find(params[:id])
-            if session.destroy!
-                render_success(session)
-            else
-                render_error(session.errors.full_messages.join('; '))
-            end
+    # POST /sessions/delete
+    def delete
+        if @session.destroy!
+            render_success @session
+        else
+            render_error @session.errors.full_messages.join('; ')
         end
+    end
 
-        private
+    private
 
-        def session_params
-            params.permit(
-                :id,
-                :name,
-                :rate1,
-                :rate2,
-                :start_date,
-                :end_date
-            )
-        end
+    def find_session
+        @session = Session.find_by(id: params[:id])
+    end
+
+    def session_params
+        params.permit(
+            :id,
+            :name,
+            :rate1,
+            :rate2,
+            :start_date,
+            :end_date
+        )
     end
 end
