77ee68a318068cab37ce3dfdde0dbf036fbc2ffa
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-10-31 23:36:00 -0400

    refactor(api): Refactored Applications controller and model

--- api/app/controllers/api/v1/applications_controller.rb
+++ api/app/controllers/api/v1/applications_controller.rb
@@ -1,138 +1,109 @@
 # frozen_string_literal: true
 
-module Api::V1
-    # Controller for Applications
-    class ApplicationsController < ApplicationController
-        # GET /applications
-        def index
-            unless params.include?(:session_id)
-                render_success(all_applications)
-                return
-            end
-            return if invalid_id(Session, :session_id)
+# Controller for Applications
+class Api::V1::ApplicationsController < ApplicationController
+    before_action :find_application, except: :index
 
-            render_success(applications_by_session)
+    # GET /applications
+    def index
+        if params[:session_id].blank?
+            render_success Application.all_applications.as_json
+            return
+        end
+        return if invalid_id?(Session, :session_id)
+
+        render_success Application.by_session
+    end
+
+    # POST /applications
+    def create
+        if params[:id].present? && @application.present?
+            update
+            return
         end
 
-        # POST /applications
-        def create
-            # if we passed in an id that exists, we want to update
-            update && return if params.key?(:id) && Application.exists?(params[:id])
-            params.require(:applicant_id)
-            return if invalid_id(Session, :session_id)
-            return if invalid_id(Applicant, :applicant_id)
+        return if invalid_id?(Applicant, :applicant_id) || invalid_id?(Session, :session_id)
 
-            application = Application.new(application_params)
-            unless application.save # does not pass Application model validation
-                application.destroy!
-                render_error(application.errors)
-                return
-            end
-            params[:application_id] = application[:id]
-            message = valid_applicant_matching_data(application.errors.messages)
-            if !message
-                render_success(application_data(application))
+        ActiveRecord::Base.transaction do
+            application = Application.create!(application_params)
+            matching = ApplicantDataForMatching.create!(applicant_data_for_matching_params)
+            error_messages = application.errors.messages.deep_merge(matching.errors.messages)
+            if !error_messages
+                render_success application.as_json
             else
-                application.destroy!
-                render_error(message)
+                render_error error_messages
             end
+        rescue StandardError
+            if application.errors? && !matching
+                render_error application.errors
+            elsif matching.errors?
+                render_error matching.errors
+            else
+                render_error 'Something went wrong'
+            end
+            raise ActiveRecord::Rollback
         end
+    end
 
-        def update
-            application = Application.find(params[:id])
-            matching = application.applicant_data_for_matching
-            application_res = application.update_attributes!(application_update_params)
-            matching_res = matching.update_attributes!(matching_data_update_params)
+    def update
+        matching = @application.applicant_data_for_matching
+        ActiveRecord::Base.transaction do
+            application = application.update_attributes!(application_update_params)
+            matching = matching.update_attributes!(matching_data_update_params)
+            render_success application.as_json
+        rescue StandardError
             errors = application.errors.messages.deep_merge(matching.errors.messages)
-            if application_res && matching_res
-                render_success(application_data(application))
-            else
-                render_error(errors)
-            end
+            render_error errors
         end
+    end
 
-        # POST /applications/delete
-        def delete
-            params.require(:id)
-            application = Application.find(params[:id])
-            entry = application_data(application)
-            if application
-                matching = application.applicant_data_for_matching
-                matching.destroy!
-            end
-            if application.destroy!
-                render_success(entry)
-            else
-                render_error(application.errors.full_messages.join('; '))
-            end
+    # POST /applications/delete
+    def delete
+        if @application.destroy!
+            render_success @application.as_json
+        else
+            render_error @application.errors.full_messages.join('; ')
         end
+    end
 
-        private
+    private
 
-        def application_params
-            params.permit(
-                :comments,
-                :session_id
-            )
-        end
+    def find_application
+        @application = Application.find_by(id: params[:id])
+    end
 
-        def applicant_data_for_matching_params
-            params.permit(
-                :program,
-                :department,
-                :previous_uoft_ta_experience,
-                :yip,
-                :annotation,
-                :applicant_id,
-                :application_id
-            )
-        end
+    def application_params
+        params.permit(
+            :comments,
+            :session_id
+        )
+    end
 
-        def application_update_params
-            params.permit(
-                :comments
-            )
-        end
+    def applicant_data_for_matching_params
+        params.permit(
+            :program,
+            :department,
+            :previous_uoft_ta_experience,
+            :yip,
+            :annotation,
+            :applicant_id,
+            :application_id
+        )
+    end
 
-        def matching_data_update_params
-            params.permit(
-                :program,
-                :department,
-                :previous_uoft_ta_experience,
-                :yip,
-                :annotation
-            )
-        end
+    def application_update_params
+        params.permit(
+            :comments
+        )
+    end
 
-        def applications_by_session
-            all_applications.select do |entry|
-                entry[:session_id] == params[:session_id].to_i
-            end
-        end
-
-        def all_applications
-            Application.order(:id).map do |entry|
-                application_data(entry)
-            end
-        end
-
-        def application_data(application)
-            exclusion = %i[id created_at updated_at application_id]
-            matching = application.applicant_data_for_matching
-            matching = json(matching, except: exclusion)
-            json(application, include: matching)
-        end
-
-        def valid_applicant_matching_data(errors)
-            return if invalid_id(Applicant, :applicant_id)
-
-            matching = ApplicantDataForMatching.new(applicant_data_for_matching_params)
-            if matching.save
-                return nil
-            else
-                matching.destroy!
-                return errors.deep_merge(matching.errors.messages)
-            end
-        end
+    def matching_data_update_params
+        params.permit(
+            :program,
+            :department,
+            :previous_uoft_ta_experience,
+            :yip,
+            :annotation
+        )
     end
 end
--- api/app/models/application.rb
+++ api/app/models/application.rb
@@ -3,10 +3,19 @@
 # A class representing an application of an applicant.
 class Application < ApplicationRecord
     belongs_to :session
-    has_one :applicant_data_for_matching
+    has_one :applicant_data_for_matching, dependent: :destroy
     has_one :applicant, through: :applicant_data_for_matching
     has_many :position_preferences
     has_many :positions, through: :position_preferences
+
+    JSON_EXCEPTIONS = %i[id created_at updated_at application_id].freeze
+
+    scope :all_applications, -> { includes(:applicant_data_for_matching).all.order(:id) }
+    scope :by_session, ->(session_id) { all_applications.where(session_id: session_id) }
+
+    def as_json(_options = {})
+        super(except: JSON_EXCEPTIONS, include: { applicant_data_for_matching: { except: JSON_EXCEPTIONS } })
+    end
 end
 
 # == Schema Information
