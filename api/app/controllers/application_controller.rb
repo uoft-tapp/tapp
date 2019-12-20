411867b66b1486fc84a086872779edee919ef090
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-12-20 02:00:39 -0500

    Disable param wrapping -- treat params as is

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::API
     include Response
     include ExceptionHandler
     include TransactionHandler
+
+    # Don't wrap parameters
+    # https://guides.rubyonrails.org/action_controller_overview.html#json-xml-parameters
+    wrap_parameters false
 end
