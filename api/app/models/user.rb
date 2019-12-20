1f5946f5d29f2d3b68df9993cd8782db065c5c46
Stephen Tsimicalis <tsimica@users.noreply.github.com>
2019-12-20 01:47:02 -0500

    Remove User role enum

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,7 +3,6 @@
 # A class representing a user. This holds information regarding known users and their roles
 # to determine what they should be allowed access to.
 class User < ApplicationRecord
-    enum role: %i[admin instructor]
     validates_presence_of :utorid
     validates_uniqueness_of :utorid
 end
--- api/db/migrate/20191108193619_create_users.rb
+++ api/db/migrate/20191108193619_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     def change
         create_table :users do |t|
             t.string :utorid
-            t.integer :role
             t.timestamps
         end
         add_index :users, :utorid, unique: true
