b19012e5e9965d8a16788503b3779ffee2614e74
SHIYI <petertao9t@gmail.com>
2020-04-09 21:55:44 -0400

    skip the real api session test for further modification

--- frontend/src/tests/api.test.js
+++ frontend/src/tests/api.test.js
@@ -24,7 +24,7 @@ describe("API tests", () => {
         await databaseSeeder.verifySeed(api);
     }, 30000);
 
-    describe("`/sessions` tests", () => {
+    describe.skip("`/sessions` tests", () => {
         sessionsTests(api);
     });
 
