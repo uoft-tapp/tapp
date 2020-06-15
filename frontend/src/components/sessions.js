f70e1dbab67109bcf14ff8190a33550af987377d
Denny Wiseman <wisemandenny@gmail.com>
2020-06-15 12:25:54 -0400

    sessions view dates are now human-readable format

--- frontend/src/components/sessions.js
+++ frontend/src/components/sessions.js
@@ -9,9 +9,7 @@ import { EditableField } from "./edit-field-widgets";
 const DEFAULT_COLUMNS = [
     { Header: "Name", accessor: "name" },
     { Header: "Start", accessor: "start_date" },
-    {
-        Header: "End", accessor: "end_date"
-    },
+    { Header: "End", accessor: "end_date" },
     { Header: "Rate (Pre-January)", accessor: "rate1" },
     { Header: "Rate (Post-January)", accessor: "rate2" },
 ];
@@ -21,25 +19,6 @@ function _formatDate(dateString) {
     return `${date.toLocaleDateString("en-US", { month: 'long', year: 'numeric', day: 'numeric' })}`;
 }
 
-/**
- * A cell that renders editable rate information
- *
- * @param {*} props
- * @returns
- */
-function EditableCell(title, value, onChange) {
-    return (
-        <EditableField title={title} value={value} onChange={onChange}>
-            {value}
-        </EditableField>
-    );
-    // const title = `Edit ${props.column.Header}`;
-    // const { upsertInstructor, field } = props;
-    // function onChange(newVal) {
-    //     const applicantId = props.original.id;
-    //     upsertInstructor({ id: applicantId, [field]: newVal });
-    // }
-}
 
 /**
  * List the sessions using a ReactTable. `columns` can be passed
@@ -58,16 +37,8 @@ export function SessionsList(props) {
                 ...session,
                 start_date: _formatDate(session.start_date),
                 end_date: _formatDate(session.end_date),
-                rate1: EditableCell(
-                    "Rate (pre-January)",
-                    session.rate1,
-                    undefined
-                ),
-                rate2: EditableCell(
-                    "Rate (post-January)",
-                    session.rate2,
-                    undefined
-                ),
+                rate1: session.rate1,
+                rate2: session.rate2
             }))
             : sessions;
 
