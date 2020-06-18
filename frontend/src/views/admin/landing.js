faf5cf4d9d3f4aaca20e0cef719a27fe9e7a363f
Denny Wiseman <wisemandenny@gmail.com>
2020-06-18 13:00:04 -0400

    added a jumbotron for no active session

--- frontend/src/views/admin/landing.js
+++ frontend/src/views/admin/landing.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Jumbotron, Button } from "react-bootstrap";
+import { activeSessionSelector } from "../../api/actions";
 
-function Landing() {
+function NoActiveSessionJumbotron() {
+    return (
+        <Jumbotron>
+            <h1>Welcome to TAPP!</h1>
+            <p>
+                There is <strong>no active session</strong> selected.
+            </p>
+            <p>Choose an active session from the selected session menu.</p>
+            <p>
+                <Button variant="primary">Learn more about TAPP</Button>
+            </p>
+        </Jumbotron>
+    );
+}
+function Landing(props) {
+    if (props.activeSession === null) {
+        return NoActiveSessionJumbotron();
+    }
     return <div>This is a landing page.</div>;
 }
 
-export { Landing };
+const mapActiveSessionStateToProps = (state) => ({
+    activeSession: activeSessionSelector(state),
+});
+
+const ConnectedLandingView = connect(mapActiveSessionStateToProps)(Landing);
+export { ConnectedLandingView as Landing };
