c2a5cec8df6e313ef91f027e8ac7563fde6ae120
Jason Siefken <siefkenj@gmail.com>
2020-06-25 14:15:16 -0400

    Stubbed out offer tests

--- frontend/src/tests/offer-tests.js
+++ frontend/src/tests/offer-tests.js
@@ -7,7 +7,11 @@ export function offersTests({ apiGET, apiPOST }) {
     it.todo("get offers for session");
     // maybe we don't need this in the API?
     it.todo("get offers for position");
+
     it.todo("get offer for assignment");
+    it.todo("create an offer from an assignment");
+    it.todo("error when attempting to create an offer for an assignment that has an active offer that is accepted/rejected/pending");
+    it.todo("create an offer for an assignment that has an active offer that is withdrawn/preliminary");
     it.todo("accept/reject/withdraw offer");
     it.todo("increment nag count");
     it.todo("error when attempting to update a frozen field");
