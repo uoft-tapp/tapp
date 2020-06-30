896834cddabbc044c233b7fb788a4df2c845e9b6
Denny Wiseman <wisemandenny@gmail.com>
2020-06-30 12:47:38 -0400

    added simple/advanced toggle to positions view

--- frontend/src/views/positions/index.js
+++ frontend/src/views/positions/index.js
@@ -1,19 +1,52 @@
 import React from "react";
 import { ConnectedAddPositionDialog } from "./add-position-dialog";
-import { Button } from "react-bootstrap";
+import {
+    Button,
+    ButtonToolbar,
+    ButtonGroup,
+    ToggleButtonGroup,
+    ToggleButton,
+} from "react-bootstrap";
 import { ConnectedPositionsList } from "./position-list";
 
 export function AdminPositionsView() {
     const [addDialogVisible, setAddDialogVisible] = React.useState(false);
+    const [advancedView, setAdvancedView] = React.useState("simple");
     return (
         <div>
-            <Button
-                onClick={() => {
-                    setAddDialogVisible(true);
-                }}
-            >
-                Add Position
-            </Button>
+            <ButtonToolbar className="d-flex justify-content-between px-3 py-2">
+                <ButtonGroup>
+                    <Button
+                        onClick={() => {
+                            setAddDialogVisible(true);
+                        }}
+                    >
+                        Add Position
+                    </Button>
+                </ButtonGroup>
+                <ToggleButtonGroup
+                    type="radio"
+                    value={advancedView}
+                    name="advancedView"
+                    onChange={(val) => setAdvancedView(val)}
+                >
+                    <ToggleButton
+                        className="toggle-button"
+                        value="simple"
+                        variant="light"
+                    >
+                        Simple View
+                    </ToggleButton>
+                    <ToggleButton
+                        className="toggle-button"
+                        value="advanced"
+                        variant="light"
+                    >
+                        Advanced View
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </ButtonToolbar>
+
             <ConnectedAddPositionDialog
                 show={addDialogVisible}
                 onHide={() => {
