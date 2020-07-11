fe8c1577fde657bb7b1876bc40b6ed37c94043c8
Denny Wiseman <wisemandenny@gmail.com>
2020-07-11 14:43:42 -0400

    added action/content area to sessions view

--- frontend/src/views/sessions/index.js
+++ frontend/src/views/sessions/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { ConnectedAddSessionDialog } from "./add-session-dialog";
-import { Button } from "react-bootstrap";
 import { sessionsSelector } from "../../api/actions";
 import { SessionsList } from "../../components/sessions";
+import {
+    ActionsList,
+    ActionButton,
+    ActionHeader,
+} from "../../components/action-buttons";
+import { ContentArea } from "../../components/layout";
+import { FaPlusCircle } from "react-icons/fa";
 
 const ConnectedSessionList = connect((state) => ({
     sessions: sessionsSelector(state),
@@ -12,21 +18,27 @@ const ConnectedSessionList = connect((state) => ({
 export function AdminSessionsView() {
     const [addDialogVisible, setAddDialogVisible] = React.useState(false);
     return (
-        <div>
-            <Button
-                onClick={() => {
-                    setAddDialogVisible(true);
-                }}
-            >
-                Add Session
-            </Button>
-            <ConnectedAddSessionDialog
-                show={addDialogVisible}
-                onHide={() => {
-                    setAddDialogVisible(false);
-                }}
-            />
-            <ConnectedSessionList />
+        <div className="page-body">
+            <ActionsList>
+                <ActionHeader>Available Actions</ActionHeader>
+                <ActionButton
+                    icon={<FaPlusCircle />}
+                    onClick={() => {
+                        setAddDialogVisible(true);
+                    }}
+                >
+                    Add Session
+                </ActionButton>
+            </ActionsList>
+            <ContentArea>
+                <ConnectedAddSessionDialog
+                    show={addDialogVisible}
+                    onHide={() => {
+                        setAddDialogVisible(false);
+                    }}
+                />
+                <ConnectedSessionList />
+            </ContentArea>
         </div>
     );
 }
