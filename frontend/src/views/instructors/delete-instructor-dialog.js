14df66887d9aeef49ddc1f051974a56ab06d4aae
Denny Wiseman <wisemandenny@gmail.com>
2020-07-14 14:30:37 -0400

    minor refactor for delete instructor dialog

--- frontend/src/views/instructors/delete-instructor-dialog.js
+++ frontend/src/views/instructors/delete-instructor-dialog.js
@@ -3,24 +3,30 @@ import { Modal, Button } from "react-bootstrap";
 
 export function DeleteInstructorDialog(props) {
     const { show, onHide, onDelete, instructor } = props;
-    return !instructor ? null : ( // needed to catch the error of null instructor
-        <Modal show={show} onHide={onHide}>
-            <Modal.Header closeButton>
-                <Modal.Title>Delete Instructor</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                Are you sure you want to delete instructor{" "}
-                {`${instructor.last_name}, ${instructor.first_name}`}? This
-                action cannot be undone.
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={onHide} variant="light">
-                    Cancel
-                </Button>
-                <Button onClick={onDelete} title="Delete Instructor">
-                    Delete
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+    if (!instructor) {
+        // This check ensures that the instructor object exists before trying to access instructor.last_name
+        // and instructor.first_name, which will cause a runtime error if instructor is null.
+        return null;
+    } else {
+        return (
+            <Modal show={show} onHide={onHide}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete Instructor</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure you want to delete instructor{" "}
+                    {`${instructor.last_name}, ${instructor.first_name}`}? This
+                    action cannot be undone.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={onHide} variant="light">
+                        Cancel
+                    </Button>
+                    <Button onClick={onDelete} title="Delete Instructor">
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
 }
