02221d34bb9fe2ba1ef168a6b4f8fad4b4e6f745
reidka <reid@cs.toronto.edu>
2020-07-23 12:00:12 -0400

    fixing rubocop complaints

--- backend/app/mailers/offer_mailer.rb
+++ backend/app/mailers/offer_mailer.rb
@@ -1,13 +1,12 @@
+# frozen_string_literal: true
+
 class OfferMailer < ApplicationMailer
-  # default from comes from config environment
-  layout 'mailer'
+    # default from comes from config environment
+    layout 'mailer'
 
-  def contract_email(assignment, link)
-    @offer = assignment.active_offer
-    @url = link
-    puts("In contract_email")
-    mail(to: assignment.applicant.email, subject: 'hi there')
-    
-  end
-
+    def contract_email(assignment, link)
+        @offer = assignment.active_offer
+        @url = link
+        mail(to: assignment.applicant.email, subject: 'hi there')
+    end
 end
