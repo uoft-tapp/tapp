7c9502af1c8f69038c885e3357431bafea885147
Osama <osama.hafez@mail.utoronto.ca>
2021-01-22 15:14:22 -0500

    initial implementation for assignment insertion with null dates [WIP]

--- frontend/src/tests/assignment-tests.js
+++ frontend/src/tests/assignment-tests.js
@@ -128,7 +128,54 @@ export function assignmentsTests(api) {
         expect(withUpdatedAssignment.length).toEqual(2);
     });
 
-    it.todo(
-        "assignments created with null start/end_date inherit the start/end_date from the parent position"
-    );
+    it("assignments created with null start/end_date inherit the start/end_date from the parent position", async () => {
+        // create a new position to assign
+        const newPositionData = {
+            position_code: "CSC100F",
+            position_title: "Basic Computer Science",
+            hours_per_assignment: 70,
+            start_date: "2019/09/09",
+            end_date: "2019/12/31",
+            contract_template_id: contractTemplate.id,
+        };
+
+        const { payload: position } = await apiPOST(
+            `/admin/sessions/${session.id}/positions`,
+            newPositionData
+        );
+
+        // create new assignment with no start or end date.
+        const newAssignmentData = {
+            note: "",
+            position_id: position.id,
+            applicant_id: applicant.id,
+            start_date: null,
+            end_date: null,
+        };
+        const resp = await apiPOST("/admin/assignments", newAssignmentData);
+
+        expect(resp).toHaveStatus("success");
+        const { payload: createdAssignment } = resp; //I have a lot of questions about this line.
+        // make sure the propTypes are right
+        checkPropTypes(assignmentPropTypes, createdAssignment);
+        expect(createdAssignment.id).not.toBeNull();
+        expect(createdAssignment.id).not.toEqual(assignment.id);
+        expect(createdAssignment.start_date).toEqual(newPositionData.start_date);
+        expect(createdAssignment.end_date).toEqual(newPositionData.end_date);
+
+        // fetch all assignments and make sure the newly added
+        // assignment data is there
+        const { payload: withNewAssignments } = await apiGET(
+            `/admin/sessions/${session.id}/assignments`
+        );
+
+        expect(withNewAssignments.map((x) => x.id)).toContain(
+            createdAssignment.id
+        );
+        expect(
+            withNewAssignments.filter((s) => s.id === createdAssignment.id)
+        ).toContainObject(createdAssignment);
+
+        expect(withNewAssignments.length).toEqual(2);
+    });
 }
