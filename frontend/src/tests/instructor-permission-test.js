1d2bdb376f89222b7bde2a073523c3dcca6d1052
Junan Zhao <acceleratorsnow@gmail.com>
2021-01-22 15:20:06 -0500

    Fixed formatting issues in instructor-permission-test.js

--- frontend/src/tests/instructor-permission-test.js
+++ frontend/src/tests/instructor-permission-test.js
@@ -1,4 +1,4 @@
-import {it, expect, beforeAll, checkPropTypes, errorPropTypes} from "./utils";
+import { it, expect, beforeAll, checkPropTypes, errorPropTypes } from "./utils";
 import { databaseSeeder } from "./setup";
 
 /**
@@ -62,15 +62,12 @@ export function instructorsPermissionTests(api) {
 
     it("can't update session", async () => {
         const testSessionData = {
-            "start_date": "2020-02-10T00:00:00.000Z",
-            "end_date": "2020-02-10T00:00:00.000Z",
-            "name": "test name"
+            start_date: "2020-02-10T00:00:00.000Z",
+            end_date: "2020-02-10T00:00:00.000Z",
+            name: "test name",
         };
 
-        let resp = await apiPOST(
-            `/instructor/sessions`,
-            testSessionData
-        );
+        let resp = await apiPOST(`/instructor/sessions`, testSessionData);
 
         expect(resp).toHaveStatus("error");
         checkPropTypes(errorPropTypes, resp);
@@ -83,35 +80,29 @@ export function instructorsPermissionTests(api) {
 
     it("can't update position", async () => {
         const testPositionData = {
-            "position_code": "CSC494",
-            "position_title": "Capstone Project",
-            "hours_per_assignment": 0,
-            "start_date": "2020-02-10T00:00:00.000Z",
-            "end_date": "2020-02-10T00:00:00.000Z",
-            "duties": "test duty",
-            "qualifications": "test qualification",
-            "ad_hours_per_assignment": 0,
-            "ad_num_assignments": 0,
-            "ad_open_date": "test date",
-            "ad_close_date": "test date",
-            "desired_num_assignments": 0,
-            "current_enrollment": 0,
-            "current_waitlisted": 0,
-            "instructor_ids": [
-                0,
-                "test id"
-            ],
-            "instructor_preference": [
+            position_code: "CSC494",
+            position_title: "Capstone Project",
+            hours_per_assignment: 0,
+            start_date: "2020-02-10T00:00:00.000Z",
+            end_date: "2020-02-10T00:00:00.000Z",
+            duties: "test duty",
+            qualifications: "test qualification",
+            ad_hours_per_assignment: 0,
+            ad_num_assignments: 0,
+            ad_open_date: "test date",
+            ad_close_date: "test date",
+            desired_num_assignments: 0,
+            current_enrollment: 0,
+            current_waitlisted: 0,
+            instructor_ids: [0, "test id"],
+            instructor_preference: [
                 {
-                    "preference_level": 0
-                }
-            ]
+                    preference_level: 0,
+                },
+            ],
         };
 
-        let resp = await apiPOST(
-            `/instructor/positions`,
-            testPositionData
-        );
+        let resp = await apiPOST(`/instructor/positions`, testPositionData);
 
         expect(resp).toHaveStatus("error");
         checkPropTypes(errorPropTypes, resp);
@@ -126,8 +117,8 @@ export function instructorsPermissionTests(api) {
 
     it("can't update contract template", async () => {
         const testTemplateData = {
-            "file_name": "test file name",
-            "content": "test content"
+            file_name: "test file name",
+            content: "test content",
         };
 
         let resp = await apiPOST(
@@ -141,8 +132,8 @@ export function instructorsPermissionTests(api) {
 
     it("can't upload contract template", async () => {
         const testTemplateData = {
-            "file_name": "test file name",
-            "content": "test content"
+            file_name: "test file name",
+            content: "test content",
         };
 
         let resp = await apiPOST(
@@ -163,12 +154,12 @@ export function instructorsPermissionTests(api) {
 
     it("can't update applicants", async () => {
         const testApplicantData = {
-            "utorid": "test utorid",
-            "student_number": "test student num",
-            "first_name": "test first name",
-            "last_name": "test last name",
-            "email": "test email",
-            "phone": "test phone num"
+            utorid: "test utorid",
+            student_number: "test student num",
+            first_name: "test first name",
+            last_name: "test last name",
+            email: "test email",
+            phone: "test phone num",
         };
 
         let resp = await apiPOST(
@@ -189,17 +180,17 @@ export function instructorsPermissionTests(api) {
 
     it("can't update applications", async () => {
         const testApplicationData = {
-            "comments": "test app",
-            "program": "test prog",
-            "department": "test dept",
-            "previous_uoft_experience": "test exp",
-            "yip": 0,
-            "annotation": "test ann",
-            "position_preference": [
+            comments: "test app",
+            program: "test prog",
+            department: "test dept",
+            previous_uoft_experience: "test exp",
+            yip: 0,
+            annotation: "test ann",
+            position_preference: [
                 {
-                    "preference_level": 0
-                }
-            ]
+                    preference_level: 0,
+                },
+            ],
         };
 
         let resp = await apiPOST(
