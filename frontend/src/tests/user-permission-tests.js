113e857748064d1696114bacc48562503c657318
Junan Zhao <acceleratorsnow@gmail.com>
2021-01-26 14:10:43 -0500

    Extracted a function for switching user. Implemented instructor route tests.

--- frontend/src/tests/user-permission-tests.js
+++ frontend/src/tests/user-permission-tests.js
@@ -14,10 +14,28 @@ import { databaseSeeder } from "./setup";
 export function userPermissionsTests(api) {
     const { apiGET, apiPOST } = api;
 
-    const newUserData = {
+    const newInstructorData = {
         utorid: "userxx",
         roles: ["instructor"],
     };
+
+    async function runTestWithUser(userData, testBody) {
+        let resp = await apiPOST("/admin/users", userData);
+        // Save any extra attributes we got, such as an `id`
+        Object.assign(userData, resp.payload);
+
+        // Set the active user to the newly-created user
+        resp = await apiPOST("/debug/active_user", { id: userData.id });
+        expect(resp).toHaveStatus("success");
+
+        await testBody();
+
+        // Set the active_user back to the default.
+        resp = await apiPOST("/debug/active_user", {
+            id: databaseSeeder.seededData.active_user.id,
+        });
+        expect(resp).toHaveStatus("success");
+    }
 
     beforeAll(async () => {
         await databaseSeeder.seed(api);
@@ -30,26 +48,22 @@ export function userPermissionsTests(api) {
     });
 
     it("A non-admin cannot access the admin route", async () => {
-        let resp = await apiPOST("/admin/users", newUserData);
-        // Save any extra attributes we got, such as an `id`
-        Object.assign(newUserData, resp.payload);
-
-        // Set the active user to the newly-created user
-        resp = await apiPOST("/debug/active_user", { id: newUserData.id });
-        expect(resp).toHaveStatus("success");
-
-        // Try to fetch an admin route
-        resp = await apiGET("/admin/active_user");
-        expect(resp).toHaveStatus("error");
-
-        // Set the active_user back to the default.
-        resp = await apiPOST("/debug/active_user", {
-            id: databaseSeeder.seededData.active_user.id,
+        await runTestWithUser(newInstructorData, async () => {
+            // Try to fetch an admin route
+            let resp = await apiGET("/admin/active_user");
+            expect(resp).toHaveStatus("error");
         });
-        expect(resp).toHaveStatus("success");
     });
 
-    it.todo("An instructor can access instructor routes");
+    it("An instructor can access instructor routes", async () => {
+        // test instructor route with a user with only the instructor role
+        await runTestWithUser(newInstructorData, async () => {
+            // Try to fetch an instructor route
+            let resp = await apiGET("/instructor/instructors");
+            expect(resp).toHaveStatus("success");
+        });
+    });
+
     it.todo("A TA can access TA routes");
     it.todo(
         "An instructor who is also a TA can access instructor and TA routes"
