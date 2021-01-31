f41f80cd92dbadead10616acd338c612c06157f4
Jason Siefken <siefkenj@gmail.com>
2021-01-30 21:14:28 -0500

    Stub out reporting tags API routes

---
+++ frontend/src/api/mockAPI/reportingTags.ts
@@ -0,0 +1,67 @@
+import {
+    documentCallback,
+    wrappedPropTypes,
+    docApiPropTypes,
+} from "../defs/doc-generation";
+
+export const reportingTagRoutes = {
+    get: {
+        "/sessions/{session_id}/reporting_tags": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary: "Get all reporting tags associated with a session",
+            returns: wrappedPropTypes.arrayOf(docApiPropTypes.reportingTag),
+        }),
+        "/wage_chunks/{wage_chunk_id}/reporting_tags": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary: "Get all reporting tags associated with a wage chunk",
+            returns: wrappedPropTypes.arrayOf(docApiPropTypes.reportingTag),
+        }),
+        "/positions/{position_id}/reporting_tags": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary: "Get all reporting tags associated with a position",
+            returns: wrappedPropTypes.arrayOf(docApiPropTypes.reportingTag),
+        }),
+    },
+    post: {
+        "/wage_chunks/{wage_chunk_id}/reporting_tags": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary: "Add a reporting tag associated with a wage chunk",
+            returns: docApiPropTypes.reportingTag,
+            posts: docApiPropTypes.reportingTag,
+        }),
+        "/wage_chunks/{wage_chunk_id}/reporting_tags/delete": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary:
+                "Remove an association between a reporting tag and a wage chunk",
+            returns: docApiPropTypes.reportingTag,
+            posts: docApiPropTypes.reportingTag,
+        }),
+        "/positions/{position_id}/reporting_tags": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary: "Add a reporting tag associated with a position",
+            returns: docApiPropTypes.reportingTag,
+            posts: docApiPropTypes.reportingTag,
+        }),
+        "/positions/{position_id}/reporting_tags/delete": documentCallback({
+            func: () => {
+                throw new Error("Not implemented in mock API");
+            },
+            summary:
+                "Remove an association between a reporting tag and a position",
+            returns: docApiPropTypes.reportingTag,
+            posts: docApiPropTypes.reportingTag,
+        }),
+    },
+};
--- frontend/src/tests/reporting-tag-tests.js
+++ frontend/src/tests/reporting-tag-tests.js
@@ -8,4 +8,7 @@ export function reportingTagsTests({ apiGET, apiPOST }) {
     it.todo("get reporting_tags for wage_chunk");
     it.todo("create and delete reporting_tags for position");
     it.todo("create and delete reporting_tags for wage_chunk");
+    it.todo(
+        "when an assignment is made for a position, its wage chunks are automatically assigned any reporting tags that are associated with the position"
+    );
 }
