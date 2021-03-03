9c646c13dff024f2caa5edabf3afadcb34c7676a
Junan Zhao <acceleratorsnow@gmail.com>
2021-03-03 13:46:51 -0500

    Reformat confirmation dialog text. Add offer brief.

--- frontend/src/views/assignments/offer-actions.js
+++ frontend/src/views/assignments/offer-actions.js
@@ -19,7 +19,7 @@ import {
     FaUserPlus,
 } from "react-icons/fa";
 import { ActionButton } from "../../components/action-buttons";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 
 /**
  * Functions to test what actions you can do with a particular assignment
@@ -60,9 +60,12 @@ function OfferActionButtons(props) {
     } = props;
 
     const [show, setShow] = React.useState(false);
+    const [offerBrief, setOfferBrief] = React.useState("");
 
     const hideOfferWithdrawConfirmation = () => setShow(false);
     const showOfferWithdrawConfirmation = () => setShow(true);
+    const setMultipleOfferWithdrawBrief = (briefString) =>
+        setOfferBrief(briefString);
 
     function createOffers() {
         for (const assignment of selectedAssignments) {
@@ -71,21 +74,26 @@ function OfferActionButtons(props) {
     }
     function confirmOfferWithdraw() {
         // if withdrawing multiple offers at once, show confirmation
-        if (selectedAssignments && selectedAssignments.length > 1) {
+        if (selectedAssignments?.length > 1) {
+            let multipleOfferWithdrawBrief = "";
+
+            selectedAssignments.forEach((selectedAssignment) => {
+                multipleOfferWithdrawBrief += `${selectedAssignment.applicant.first_name} ${selectedAssignment.applicant.last_name}: ${selectedAssignment.position.position_code} (${selectedAssignment.hours} hrs)\n`;
+            });
+
+            console.log(selectedAssignments);
+            setMultipleOfferWithdrawBrief(multipleOfferWithdrawBrief);
             showOfferWithdrawConfirmation();
         } else {
             // does not need confirmation if only withdrawing one offer
-            executeConfirmedOfferWithdraw();
+            withdrawOffers();
         }
-    }
-    function executeConfirmedOfferWithdraw() {
-        withdrawOffers();
-        hideOfferWithdrawConfirmation();
     }
     function withdrawOffers() {
         for (const assignment of selectedAssignments) {
             offerForAssignmentWithdraw(assignment);
         }
+        hideOfferWithdrawConfirmation();
     }
     function emailOffers() {
         for (const assignment of selectedAssignments) {
@@ -168,19 +176,25 @@ function OfferActionButtons(props) {
             </ActionButton>
             <Modal show={show} onHide={hideOfferWithdrawConfirmation}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Caution</Modal.Title>
+                    <Modal.Title>Withdrawing Multiple Offers</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    You are withdrawing from multiple offers! Are you sure?
+                    You are withdrawing from the following{" "}
+                    {selectedAssignments?.length} offers!
+                    <br />
+                    <br />
+                    <div style={{ whiteSpace: "pre-line" }}>{offerBrief}</div>
+                    <br />
+                    Are you sure?
                 </Modal.Body>
                 <Modal.Footer>
                     <Button
                         onClick={hideOfferWithdrawConfirmation}
                         variant="light"
                     >
-                        No
+                        Cancel
                     </Button>
-                    <Button onClick={executeConfirmedOfferWithdraw}>Yes</Button>
+                    <Button onClick={withdrawOffers}>Withdraw</Button>
                 </Modal.Footer>
             </Modal>
         </React.Fragment>
