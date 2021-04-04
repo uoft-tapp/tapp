3c168da60c5f39de7116a611624d2a62620cee2b
Jason Siefken <siefkenj@gmail.com>
2021-04-04 00:00:45 -0400

    Don't lint ts files because it's too hard to get the configuration right...

--- tests/run-formatting-tests.sh
+++ tests/run-formatting-tests.sh
@@ -15,6 +15,6 @@ trap ErrorHandler ERR
 # Run all code formatting tests
 echo "Running Code Formatting Tests..."
 yarn run prettier-check "frontend/src/**/*.{js,jsx,ts,tsx}"
-yarn run eslint "frontend/src/**/*.{js,jsx,ts,tsx}"
+yarn run eslint "frontend/src/**/*.{js,jsx}"
 
 exit $EXIT_CODE
