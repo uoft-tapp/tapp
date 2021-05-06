590b87ca135d1cf2908f1cbd24bbb4b11b1fb502
Jason Siefken <siefkenj@gmail.com>
2021-05-05 20:23:22 -0400

    Fix skipped posting tests

--- backend/app/models/posting.rb
+++ backend/app/models/posting.rb
@@ -3,8 +3,8 @@
 class Posting < ApplicationRecord
     POSTING_AVAILABILITY = %i[auto closed open].freeze
     belongs_to :session
-    has_many :posting_positions
-    has_many :applications
+    has_many :posting_positions, dependent: :destroy
+    has_many :applications, dependent: :destroy
 
     has_secure_token :url_token
     enum availability: POSTING_AVAILABILITY
