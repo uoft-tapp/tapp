80c326b24ad3ac244ba9dff3d14c57d63c683869
Jason Siefken <siefkenj@gmail.com>
2021-05-13 22:26:33 -0400

    Rename seed data files

---
+++ frontend/src/mock_data/index.ts
@@ -0,0 +1,15 @@
+import applicants from "./applicants.json";
+import assignments from "./assignments.json";
+import contractTemplates from "./contract_templates.json";
+import instructors from "./instructors.json";
+import positions from "./positions.json";
+import sessions from "./sessions.json";
+
+export const seedData = {
+    applicants,
+    assignments,
+    contractTemplates,
+    instructors,
+    positions,
+    sessions,
+};
