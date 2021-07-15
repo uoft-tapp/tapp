be27298c401d1e87a025e9049d481ce48e0eeb50
developerjinchun <40876418+developerjinchun@users.noreply.github.com>
2021-07-14 22:45:55 -0400

    test for submiting survey.js data (#591)
    * test for submiting survey.js data
    
    Co-authored-by: Jin Chun <jin@Jins-MacBook-Pro.local>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Mert Kaya <47050442+mertkaya1033@users.noreply.github.com>
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Jin Chun <jinchun@Jins-MacBook-Pro.local>

--- frontend/src/tests/application-tests.js
+++ frontend/src/tests/application-tests.js
@@ -1,19 +1,112 @@
-import { it, beforeAll } from "./utils";
+import {
+    it,
+    beforeAll,
+    expect,
+    checkPropTypes,
+    postingPropTypes,
+} from "./utils";
 import { databaseSeeder } from "./setup";
 
 export function applicationsTests({ apiGET, apiPOST }) {
-    beforeAll(async () => {
-        await databaseSeeder.seed({ apiGET, apiPOST });
-    }, 30000);
+    let session;
+    let position;
+    let posting = {};
+    let adminUser;
+    let surveyData;
+
+    const taOnlyUser = {
+        utorid: "matthewc",
+        roles: ["ta"],
+    };
+
+    const postingData = {
+        name: "2021 Summer Posting",
+        intro_text: "Intro text for posting",
+        open_date: new Date("2021/05/01").toISOString(),
+        close_date: new Date("2021/08/31").toISOString(),
+        availability: "auto",
+    };
 
     // These tests set data through the `/public/postings` route,
     // but read data through the `/api/v1/admin` route.
     describe("Public route tests", () => {
+        beforeAll(async () => {
+            await databaseSeeder.seed({ apiGET, apiPOST });
+            session = databaseSeeder.seededData.session;
+            position = databaseSeeder.seededData.position;
+            let resp = await apiGET(`/debug/active_user`);
+            expect(resp).toHaveStatus("success");
+            adminUser = resp.payload;
+        });
+
         it.todo("Get survey.js posting data through public route");
         it.todo(
             "Survey.js posting data is pre-filled based on prior applicant/application data"
         );
-        it.todo("Can submit survey.js data via the public postings route");
+
+        it("Can submit survey.js data via the public postings route", async () => {
+            // Create a new posting
+            let resp = await apiPOST(
+                `/admin/sessions/${session.id}/postings`,
+                postingData
+            );
+            expect(resp).toHaveStatus("success");
+            Object.assign(posting, resp.payload);
+            checkPropTypes(postingPropTypes, posting);
+            expect(posting.id).not.toBeNull();
+
+            // Set position for posting
+            resp = await apiPOST(
+                `/admin/postings/${posting.id}/posting_positions`,
+                { position_id: position.id }
+            );
+            expect(resp).toHaveStatus("success");
+
+            // Create and switch to a ta only user
+            resp = await apiPOST("/debug/users", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+            resp = await apiPOST("/debug/active_user", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+
+            // Create and submit survey.js data
+            surveyData = {
+                answers: {
+                    utorid: "matthewc",
+                    student_number: "1000123456",
+                    first_name: "Matthew",
+                    last_name: "Cambell",
+                    email: "test@test.ca",
+                    phone: "6471234567",
+                    program: "M",
+                    program_start: "2017-09-05",
+                    department: "cs",
+                    previous_university_ta: true,
+                    some_entry: false,
+                    previous_department_ta: true,
+                    previous_other_university_ta: false,
+                    previous_experience_summary: "some previous experience",
+                    position_preferences: {
+                        [position.position_code]: 3,
+                    },
+                    comments: "some additional comments",
+                },
+            };
+
+            // Submit survey.js data
+            resp = await apiPOST(
+                `/public/postings/${posting.url_token}/submit`,
+                surveyData,
+                true
+            );
+            expect(resp).toHaveStatus("success");
+
+            // Switch back to default admin
+            resp = await apiPOST("/debug/active_user", adminUser);
+            expect(resp).toHaveStatus("success");
+
+            // Further verfication will happen in test cases concerning application data
+        });
+
         it.todo(
             "When submitting survey.js data an applicant and application are automatically created they don't exist"
         );
@@ -23,9 +116,29 @@ export function applicationsTests({ apiGET, apiPOST }) {
         it.todo(
             "Even if a different utorid is submitted via survey.js data the active_user's utorid is used"
         );
-        it.todo(
-            "When submitting survey.js data cannot add a position_preference for a position not listed in the posting"
-        );
+        it.skip("When submitting survey.js data cannot add a position_preference for a position not listed in the posting", async () => {
+            // Add illegal position's preference
+            surveyData.answers.position_preferences = {
+                ...surveyData.answers.position_preferences,
+                MAT102: 3,
+            };
+
+            // Switch to a ta only user
+            let resp = await apiPOST("/debug/active_user", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+
+            // Submit survey.js data
+            resp = await apiPOST(
+                `/public/postings/${posting.url_token}/submit`,
+                surveyData,
+                true
+            );
+            expect(resp).toHaveStatus("error");
+
+            // Switch back to default admin
+            resp = await apiPOST("/debug/active_user", adminUser);
+            expect(resp).toHaveStatus("success");
+        });
         it.todo(
             "When submitting survey.js data attached files are stored on disk rather than as base64 strings in the database"
         );
