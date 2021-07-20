3e8fe1042aac0ee8e7cfd02ba35721f09c45785e
Mert Kaya <mertkaya1033@gmail.com>
2021-07-20 15:15:56 -0400

    Resolved merge conflicts by hand

--- frontend/src/tests/application-tests.js
+++ frontend/src/tests/application-tests.js
@@ -10,7 +10,9 @@ import { databaseSeeder } from "./setup";
 
 export function applicationsTests({ apiGET, apiPOST }) {
     let session, applicant, position;
-    let defaultUser, newApplication;
+    let adminUser, newApplication;
+    let surveyData;
+
     const HIGH_PREFERENCE = 3;
     const OK_PREFERENCE = 1;
     const userCreatedFromApplicant = {};
@@ -30,6 +32,19 @@ export function applicationsTests({ apiGET, apiPOST }) {
     const userWithTaPermissions = {
         roles: ["ta"],
         utorid: "greenb",
+    };
+
+    const taOnlyUser = {
+        utorid: "matthewc",
+        roles: ["ta"],
+    };
+
+    const postingData = {
+        name: "2021 Summer Posting",
+        intro_text: "Intro text for posting",
+        open_date: new Date("2021/05/01").toISOString(),
+        close_date: new Date("2021/08/31").toISOString(),
+        availability: "auto",
     };
 
     /**
@@ -63,10 +78,7 @@ export function applicationsTests({ apiGET, apiPOST }) {
      * @returns {Promise<void>}
      */
     async function restoreDefaultUser() {
-        let respSwitchBackUser = await apiPOST(
-            `/debug/active_user`,
-            defaultUser
-        );
+        let respSwitchBackUser = await apiPOST(`/debug/active_user`, adminUser);
         expect(respSwitchBackUser).toHaveStatus("success");
     }
 
@@ -79,7 +91,7 @@ export function applicationsTests({ apiGET, apiPOST }) {
         // Get the current active user
         let resp = await apiGET("/debug/active_user");
         expect(resp).toHaveStatus("success");
-        defaultUser = resp.payload;
+        adminUser = resp.payload;
 
         // Making sure the user is the seeded applicant before this get request for the purposes of the next test
         // Created a user for the applicant to make sure they exist before changing the default user to applicant
@@ -144,7 +156,68 @@ export function applicationsTests({ apiGET, apiPOST }) {
             await restoreDefaultUser();
         });
 
-        it.todo("Can submit survey.js data via the public postings route");
+        it("Can submit survey.js data via the public postings route", async () => {
+            // Create a new posting
+            let resp = await apiPOST(
+                `/admin/sessions/${session.id}/postings`,
+                postingData
+            );
+            expect(resp).toHaveStatus("success");
+            Object.assign(posting, resp.payload);
+            checkPropTypes(postingPropTypes, posting);
+            expect(posting.id).not.toBeNull();
+
+            // Set position for posting
+            resp = await apiPOST(
+                `/admin/postings/${posting.id}/posting_positions`,
+                { position_id: position.id }
+            );
+            expect(resp).toHaveStatus("success");
+
+            // Create and switch to a ta only user
+            resp = await apiPOST("/debug/users", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+            resp = await apiPOST("/debug/active_user", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+
+            // Create and submit survey.js data
+            surveyData = {
+                answers: {
+                    utorid: "matthewc",
+                    student_number: "1000123456",
+                    first_name: "Matthew",
+                    last_name: "Cambell",
+                    email: "test@test.ca",
+                    phone: "6471234567",
+                    program: "M",
+                    program_start: "2017-09-05",
+                    department: "cs",
+                    previous_university_ta: true,
+                    some_entry: false,
+                    previous_department_ta: true,
+                    previous_other_university_ta: false,
+                    previous_experience_summary: "some previous experience",
+                    position_preferences: {
+                        [position.position_code]: 3,
+                    },
+                    comments: "some additional comments",
+                },
+            };
+
+            // Submit survey.js data
+            resp = await apiPOST(
+                `/public/postings/${posting.url_token}/submit`,
+                surveyData,
+                true
+            );
+            expect(resp).toHaveStatus("success");
+
+            // Switch back to default admin
+            resp = await apiPOST("/debug/active_user", adminUser);
+            expect(resp).toHaveStatus("success");
+
+            // Further verfication will happen in test cases concerning application data
+        });
 
         it.skip("Survey.js posting data is pre-filled based on prior application", async () => {
             // Submit an application to the posting
@@ -277,6 +350,30 @@ export function applicationsTests({ apiGET, apiPOST }) {
                 await restoreDefaultUser();
             }
         );
+
+        it.skip("When submitting survey.js data cannot add a position_preference for a position not listed in the posting", async () => {
+            // Add illegal position's preference
+            surveyData.answers.position_preferences = {
+                ...surveyData.answers.position_preferences,
+                MAT102: 3,
+            };
+
+            // Switch to a ta only user
+            let resp = await apiPOST("/debug/active_user", taOnlyUser);
+            expect(resp).toHaveStatus("success");
+
+            // Submit survey.js data
+            resp = await apiPOST(
+                `/public/postings/${posting.url_token}/submit`,
+                surveyData,
+                true
+            );
+            expect(resp).toHaveStatus("error");
+
+            // Switch back to default admin
+            resp = await apiPOST("/debug/active_user", adminUser);
+            expect(resp).toHaveStatus("success");
+        });
 
         it(
             "When submitting survey.js data an applicant and application are automatically created if they don't" +
@@ -426,9 +523,6 @@ export function applicationsTests({ apiGET, apiPOST }) {
         );
         it.todo(
             "Even if a different utorid is submitted via survey.js data the active_user's utorid is used"
-        );
-        it.todo(
-            "When submitting survey.js data cannot add a position_preference for a position not listed in the posting"
         );
         it.todo(
             "When submitting survey.js data attached files are stored on disk rather than as base64 strings in the database"
