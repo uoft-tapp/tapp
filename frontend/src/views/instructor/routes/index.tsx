78a69530e7742efe45c5c16ba6a73555a8a41d91
Alex Kozin <alexandr.job@yahoo.com>
2021-07-22 20:31:40 -0400

    Fix route redirects.

--- frontend/src/views/instructor/routes/index.tsx
+++ frontend/src/views/instructor/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import { InstructorPositionsView } from "../positions";
 import { InstructorAssignmentsView } from "../assignments";
 import { Landing as InstructorLanding } from "../landing";
 
@@ -8,15 +7,16 @@ export function InstructorRoutes() {
     return (
         <Switch>
             <Route exact path="/">
-                <Redirect to="/tapp" />
-            </Route>
-            <Route exact path="/tapp">
                 <InstructorLanding />
             </Route>
-            <Route path="/tapp/:positionId">
-                <Redirect to="/tapp/:positionId/assignments" />
-            </Route>
-            <Route path="/tapp/:positionId/assignments">
+            <Route
+                path="/:positionId"
+                render={({match}) => {
+                    const positionId = match.params.positionId;
+                    return <Redirect to={`/${positionId}/assignments`} />;
+                }}
+            />
+            <Route path="/:positionId/assignments">
                 <InstructorAssignmentsView />
             </Route>
             <Route exact path="/tapp/ddahs">
