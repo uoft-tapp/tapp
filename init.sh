ca9aa293ee66155719838386a23fa83b501e46b2
Jason Siefken <siefkenj@gmail.com>
2021-11-14 21:03:12 -0500

    Added git-blame-ignore-revs file to ignore formatting commits in git history

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ HOOKS="$TOP_LEVEL/.git/hooks"
 cp $TOP_LEVEL/resources/pre-commit $HOOKS
 
 chmod +x $HOOKS/pre-commit
+# List all formatting changes (e.g. Prettier upgrades) in .git-blame-ignore-revs
+# They will be ignored by gitblame if the commit ids are listed in that file
+git config blame.ignoreRevsFile .git-blame-ignore-revs
