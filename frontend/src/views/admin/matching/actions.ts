a3c4e4be2dcc686d2bcca4cac4adc288d430ef17
Elaine Huynh <elaine.huynh@mail.utoronto.ca>
2022-08-04 12:53:09 -0400

    matching actions now uses actionFactory

--- frontend/src/views/admin/matching/actions.ts
+++ frontend/src/views/admin/matching/actions.ts
@@ -8,37 +8,25 @@ import {
     BATCH_UPSERT_NOTES,
 } from "./constants";
 import { Match, AppointmentGuaranteeStatus } from "./types";
+import { actionFactory } from "../../../api/actions/utils";
 
 // actions
-export const upsertMatch = (data: Match) => ({
-    type: UPSERT_MATCH,
-    payload: data,
-});
+export const upsertMatch = actionFactory<Match>(UPSERT_MATCH);
 
-export const batchUpsertMatches = (data: Match[]) => ({
-    type: BATCH_UPSERT_MATCHES,
-    payload: data,
-});
+export const batchUpsertMatches = actionFactory<Match[]>(BATCH_UPSERT_MATCHES);
 
-export const upsertGuarantee = (data: AppointmentGuaranteeStatus) => ({
-    type: UPSERT_GUARANTEE,
-    payload: data,
-});
+export const upsertGuarantee =
+    actionFactory<AppointmentGuaranteeStatus>(UPSERT_GUARANTEE);
 
-export const batchUpsertGuarantees = (data: AppointmentGuaranteeStatus[]) => ({
-    type: BATCH_UPSERT_GUARANTEES,
-    payload: data,
-});
+export const batchUpsertGuarantees = actionFactory<
+    AppointmentGuaranteeStatus[]
+>(BATCH_UPSERT_GUARANTEES);
 
-export const upsertNote = (data: Record<string, string | null>) => ({
-    type: UPSERT_NOTE,
-    payload: data,
-});
+export const upsertNote =
+    actionFactory<Record<string, string | null>>(UPSERT_NOTE);
 
-export const batchUpsertNotes = (data: Record<string, string | null>) => ({
-    type: BATCH_UPSERT_NOTES,
-    payload: data,
-});
+export const batchUpsertNotes =
+    actionFactory<Record<string, string | null>>(BATCH_UPSERT_NOTES);
 
 // selectors
 export const matchingDataSelector = (state: RootState) => state.ui.matchingData;
