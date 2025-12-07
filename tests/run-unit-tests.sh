e9d5d607619da777c0403c68af38b2562d671d4e
Jason Siefken <siefkenj@gmail.com>
2025-12-07 13:37:34 -0500

    Update docs

--- tests/run-unit-tests.sh
+++ tests/run-unit-tests.sh
@@ -18,6 +18,6 @@ echo 'If they are not, please run `docker-compose up` and rerun this script.'
 # Run all tapp unit tests
 echo "Running Unit Tests..."
 # set the CI variable to true so that `npm test` doesn't run in --watch mode.
-docker-compose run -e CI=true frontend npm test
+docker compose run -e CI=true frontend npm test
 
 exit $EXIT_CODE
